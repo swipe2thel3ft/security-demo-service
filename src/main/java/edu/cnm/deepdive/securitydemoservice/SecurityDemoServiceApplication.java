bb0f8457fbe0096aa4fe754d52d2208371341e1f
Victor Perez <victorperez099@yahoo.com>
2021-07-19 09:54:15 -0600

    Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/securitydemoservice/SecurityDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.securitydemoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecurityDemoServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/securitydemoservice/SecurityDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.securitydemoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
